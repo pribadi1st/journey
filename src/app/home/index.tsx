5762327f3d69c797729df6b34dcfd4abb2c0413e
pribadi1st <pribadiridwan@rocketmail.com>
2023-11-13 21:55:13 +0100

    Fix: fix build

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,5 +1,5 @@
 'use client'
-import Laptop from './laptop';
+import Laptop from '@/app/home/Laptop';
 function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center w-full m-auto">
